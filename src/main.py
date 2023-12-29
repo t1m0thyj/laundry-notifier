afb5ec80b227b16097a4fe2d2ae3c5034ce75ffe
Timothy Johnson <tandrewjohnson@outlook.com>
2023-12-29 13:30:38 -0500

    Add file handler to improve logging

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-import logging
 import json
-import os.path
+import logging
+import os
 import re
+from logging.handlers import MemoryHandler, RotatingFileHandler
 from typing import Any, IO
 
 from notifier import LaundryNotifier
@@ -14,11 +15,18 @@ def load_json_with_comments(fileobj: IO[str]) -> Any:
     return json.loads(re.sub(comment_regex, r"", fileobj.read()))
 
 
+def init_logger(logs_dir, log_level="info"):
+    os.makedirs(logs_dir, exist_ok=True)
+    file_handler = RotatingFileHandler(os.path.join(logs_dir, "app.log"), maxBytes=1024 * 1024, backupCount=10)
+    memory_handler = MemoryHandler(1024 * 10, flushLevel=logging.ERROR, target=file_handler)
+    logging.basicConfig(level=getattr(logging, log_level.upper()), handlers=[memory_handler, logging.StreamHandler()])
+
+
 def main() -> None:
     cwd = os.path.dirname(__file__)
     with open(os.path.join(cwd, "..", "config.json"), 'r') as fileobj:
         config = load_json_with_comments(fileobj)
-    logging.basicConfig(level=getattr(logging, config["log_level"].upper()))
+    init_logger(os.path.join(cwd, "logs"), config["log_level"])
 
     notifier = LaundryNotifier(config)
     try:
